6c2d7ab652268f46d640a0a88a27ec73671217d1
Tyler Dula <dulatr@appstate.edu>
2019-04-06 22:54:46 -0400

    Clarify comments of added LFSR file

--- Tests/LFSR.c
+++ Tests/LFSR.c
@@ -8,7 +8,11 @@ a seed provided has a set cycle length before repeating.
 
 This LFSR uses the exclusive OR operator (^) for cycles.
 The cycle length has been set to be extremely long for
-various starting states.
+various starting states. 
+
+Cycle lengths are extremely sensitive to Tap locations.
+There are more than one way to apply these taps for maximal
+lengths that produce different outputs throughout the cycle.
 
 ===========================================================
 */
@@ -19,7 +23,7 @@ various starting states.
 void main(){
 
     //non zero starting seed
-    int16_t start_seed = 0xACE1;
+    int16_t start_seed = 0x0111;
     //seed the LFSR, must start different than seed for while()
     int16_t LFSR = start_seed + 0x0001;
 
@@ -27,9 +31,11 @@ void main(){
 
     while(LFSR!=start_seed){
 
+        //'Taps' are placed at bit positions 7,9,13 of a 16bit input
         LFSR ^= LFSR >> 7;  
         LFSR ^= LFSR << 9;
         LFSR ^= LFSR >> 13;     
+        
         counter++;
     }
     printf("%d\n",counter);
